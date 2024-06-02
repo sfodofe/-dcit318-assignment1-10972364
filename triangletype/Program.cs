d213447406dbd9380574a99f14c517dd66f0617c
sfodofe <sfodofe@st.ug.edu.gh>
2024-06-02 22:28:02 +0000

    triangle type identifier

---
+++ triangletype/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TriangleType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side 2: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side 3: ");
+            double side3 = Convert.ToDouble(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("Equilateral triangle");
+            }
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                Console.WriteLine("Isosceles triangle");
+            }
+            else
+            {
+                Console.WriteLine("Scalene triangle");
+            }
+        }
+    }
+}
