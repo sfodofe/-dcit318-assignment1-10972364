18ff63d6363cfa27c5873cf2717de5b3f19126c1
sfodofe <sfodofe@st.ug.edu.gh>
2024-06-02 22:23:43 +0000

    A grade Calculator

---
+++ agradecalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade (0-100): ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Letter grade: A");
+            }
+            else if (grade >= 80 && grade <= 89)
+            {
+                Console.WriteLine("Letter grade: B");
+            }
+            else if (grade >= 70 && grade <= 79)
+            {
+                Console.WriteLine("Letter grade: C");
+            }
+            else if (grade >= 60 && grade <= 69)
+            {
+                Console.WriteLine("Letter grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Letter grade: F");
+            }
+        }
+    }
+}
